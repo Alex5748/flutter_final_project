a2e62135269b62bcd0337022af714f21bd4c7a5d
Yukesh <yukesh@MacBook-Pro.local>
2022-07-05 12:04:14 +0545

    final

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_final_project/productdetails.dart';
 import 'package:flutter_final_project/profile.dart';
 import 'package:flutter_final_project/bottom_nav.dart';
+import 'package:flutter_final_project/services/network_helper.dart';
 
 class Home extends StatefulWidget {
   final String? data;
@@ -14,6 +15,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var posts;
+  @override
+  void initState() {
+    super.initState();
+    MApidata();
+  }
+
+  MApidata() async {
+    posts = await NetworkHelper().getAPIResponse();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,42 +38,83 @@ class _HomeState extends State<Home> {
         // backgroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 10, 30, 0),
-            child: Row(
-              children: [
-                Text(
-                  'Good Afternoon, User',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 10, 30, 0),
+              child: Row(
+                children: [
+                  Text(
+                    'Good Afternoon, User',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(17, 0, 13, 0),
-            child: Image(
-              image: AssetImage('asset/covidbanner.png'),
-              height: 300,
-              width: 350,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(17, 0, 13, 0),
+              child: Image(
+                image: AssetImage('asset/covidbanner.png'),
+                height: 300,
+                width: 350,
+              ),
             ),
-          ),
-          Card(
-            child: Column(
-              children: [
-                Text(widget.name.toString()),
-                Column(
-                  children: [
-                    Text(widget.data.toString()),
-                    Text(widget.price.toString().toString()),
-                  ],
-                ),
-                Icon(Icons.shopping_cart_checkout),
-              ],
-            ),
-          )
-        ],
+            posts != null
+                ? ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Row(
+                          // crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Image.network(
+                              'http://mark.bslmeiyu.com/uploads/${posts[index].img}',
+                              height: 100,
+                              width: 100,
+                            ),
+                            Spacer(),
+                            Text(posts[index].name),
+                            Spacer(),
+                            IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    // Navigator.push(context,
+                                    //     MaterialPageRoute(builder: (context) {
+                                    //   return TestPage(
+                                    //     getData: posts[index].description,
+                                    //     starData: posts[index].stars,
+                                    //     imgData: posts[index].img,
+                                    //     priceData: posts[index].price,
+                                    //     nameData: posts[index].name,
+                                    //     locData: posts[index].location,
+                                    //   );
+                                    // }));
+                                  });
+                                },
+                                icon: Icon(Icons.forward))
+                          ],
+                        ),
+                      );
+                    })
+                : Center(child: CircularProgressIndicator()),
+            // Card(
+            //   child: Column(
+            //     children: [
+            //       Text(widget.name.toString()),
+            //       Column(
+            //         children: [
+            //           Text(widget.data.toString()),
+            //           Text(widget.price.toString().toString()),
+            //         ],
+            //       ),
+            //       Icon(Icons.shopping_cart_checkout),
+            //     ],
+            //   ),
+            // )
+          ],
+        ),
       ),
     );
   }
